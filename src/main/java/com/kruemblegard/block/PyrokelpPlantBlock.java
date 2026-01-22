829bd809d40133d21fe205b8646b9355151aeee2
smokydstona <smokydstona@gmail.com>
2026-01-22 10:22:20 -0800

    Pyrokelp: bonemeal like twisting vines

--- src/main/java/com/kruemblegard/block/PyrokelpPlantBlock.java
+++ src/main/java/com/kruemblegard/block/PyrokelpPlantBlock.java
@@ -5,9 +5,13 @@ import com.kruemblegard.registry.ModTags;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.GrowingPlantBodyBlock;
 import net.minecraft.world.level.block.GrowingPlantHeadBlock;
@@ -25,6 +29,49 @@ public class PyrokelpPlantBlock extends GrowingPlantBodyBlock {
     @Override
     protected GrowingPlantHeadBlock getHeadBlock() {
         return (GrowingPlantHeadBlock) ModBlocks.PYROKELP.get();
+    }
+
+    private BlockPos findHeadPos(LevelReader level, BlockPos startPos) {
+        BlockPos cursor = startPos;
+        while (level.getBlockState(cursor).is(this)) {
+            cursor = cursor.above();
+        }
+        return cursor;
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state, boolean isClient) {
+        if (!level.getBiome(pos).is(ModTags.WorldgenBiomes.BASIN_OF_SCARS)) {
+            return false;
+        }
+
+        BlockPos headPos = findHeadPos(level, pos);
+        BlockState headState = level.getBlockState(headPos);
+        if (!(headState.getBlock() instanceof BonemealableBlock bonemealable)) {
+            return false;
+        }
+
+        return bonemealable.isValidBonemealTarget(level, headPos, headState, isClient);
+    }
+
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
+        if (!level.getBiome(pos).is(ModTags.WorldgenBiomes.BASIN_OF_SCARS)) {
+            return;
+        }
+
+        BlockPos headPos = findHeadPos(level, pos);
+        BlockState headState = level.getBlockState(headPos);
+        if (!(headState.getBlock() instanceof BonemealableBlock bonemealable)) {
+            return;
+        }
+
+        bonemealable.performBonemeal(level, random, headPos, headState);
     }
 
     @Override
