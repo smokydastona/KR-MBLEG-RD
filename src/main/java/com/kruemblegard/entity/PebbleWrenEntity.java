9c79e9492119c41f8584f5322a3f84fb114b92fe
smokydstona <smokydstona@gmail.com>
2026-03-21 12:09:21 -0700

    Fix Mojmap imports for Pebble Wren and Mossback Tortoise

--- src/main/java/com/kruemblegard/entity/PebbleWrenEntity.java
+++ src/main/java/com/kruemblegard/entity/PebbleWrenEntity.java
@@ -22,7 +22,7 @@ import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.SitWhenOrderedToGoal;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
-import net.minecraft.world.entity.animal.TamableAnimal;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
